5fc82e52c135d870c8fa4de920bb9364c0583fcd
supreetha2003 <suprithaur@gmail.com>
2025-04-21 18:53:01 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,29 @@
+package firstassesment;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner Scanner = new Scanner (System.in);
+		System.out.println("choose your option");
+		number=Scanner.nextInt();
+		System.out.println("the call Warfare is:",1500);
+		System.out.println("Speed Races:",1200);
+		
+		System.out.println("Mystery mansion:",1000);
+		System.out.println("pixel Adventure:",800);
+		System.out.println("Puzzle:",500);
+		
+		
+		
+	
+
+	}
+
+}
+1. Call of Warfare   - ₹1500   
+2. Speed Racers      - ₹1200   
+3. Mystery Mansion   - ₹1000   
+4. Pixel Adventure   - ₹800   
+5. Puzzle Mania      - ₹500   
+6. Checkout / Exit 
