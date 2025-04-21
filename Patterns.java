e0dc74194d582c10b172ada6a91825ceb0959e06
supreetha2003 <suprithaur@gmail.com>
2025-04-21 18:52:41 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,19 @@
+package firstassesment;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=0;i++) {
+			System.out.println("*");
+			for(int j=3;j<=0;j++);
+			System.out.println("***");
+			for(int k=0;k<=0;k++) {
+				System.out.println("******");
+			}
+			
+		}
+	}
+}
+		
+			
+
